5ba609b2e99434944848f5ee9097126d9caf347b
Pawel-Rugala <webdevpablo@gmail.com>
2021-05-14 16:23:02 +0200

    FEAT: Clean webpack build for Dev mode added

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,6 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'development',
@@ -25,4 +26,15 @@ module.exports = merge(common, {
       },
     ],
   },
+  plugins: [
+    new CleanWebpackPlugin({
+      // Simulate the removal of files
+      dry: true,
+      // Write Logs to Console
+      verbose: true,
+      // Automatically remove all unused webpack assets on rebuild
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false,
+    }),
+  ],
 });
